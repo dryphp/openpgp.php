951ff2cacc857a42f310c5be3503dfdb845c9bb7
Stephen Paul Weber <singpolyma@singpolyma.net>
2011-07-25 11:50:27 -0500

    use bitlength

--- lib/openpgp.php
+++ lib/openpgp.php
@@ -476,7 +476,7 @@ class OpenPGP_SignaturePacket extends OpenPGP_Packet {
     $body .= pack('n', strlen($unhashed_subpackets)).$unhashed_subpackets;
 
     $body .= pack('n', $this->hash_head);
-    $body .= pack('n', strlen($this->data)*8).$this->data;
+    $body .= pack('n', OpenPGP::bitlength($this->data)).$this->data;
 
     return $body;
   }
