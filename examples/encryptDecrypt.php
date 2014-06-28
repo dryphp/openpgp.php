775aa9611882673aa0c36596c198e526065c2593
Stephen Paul Weber <singpolyma@singpolyma.net>
2014-06-28 11:38:14 -0500

    Remove debug code

--- examples/encryptDecrypt.php
+++ examples/encryptDecrypt.php
@@ -8,8 +8,6 @@ $key = OpenPGP_Message::parse(file_get_contents(dirname(__FILE__) . '/../tests/d
 $data = new OpenPGP_LiteralDataPacket('This is text.', array('format' => 'u', 'filename' => 'stuff.txt'));
 $encrypted = OpenPGP_Crypt_Symmetric::encrypt($key, new OpenPGP_Message(array($data)));
 
-echo $encrypted->to_bytes();exit;
-
 // Now decrypt it with the same key
 $decryptor = new OpenPGP_Crypt_RSA($key);
 $decrypted = $decryptor->decrypt($encrypted);
